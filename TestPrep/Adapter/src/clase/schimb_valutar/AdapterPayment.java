09b24f967d186afc52671920d836be24db6180e9
Alex <georgealexandrusurugiu@gmail.com>
2025-05-23 12:06:51 +0300

    refactor

--- TestPrep/Adapter/src/clase/schimb_valutar/AdapterPayment.java
+++ TestPrep/Adapter/src/clase/schimb_valutar/AdapterPayment.java
@@ -3,6 +3,7 @@ package clase.schimb_valutar;
 public class AdapterPayment extends NewPaymentSystem implements IOldPaymentSystem {
     @Override
     public void payInUSD(double amount) {
-        this.payInEUR(amount);
+        double euroAmount = amount*0.89;
+        this.payInEUR(euroAmount);
     }
 }
--- TestPrep/Adapter/src/clase/schimb_valutar/NewPaymentSystem.java
+++ TestPrep/Adapter/src/clase/schimb_valutar/NewPaymentSystem.java
@@ -3,7 +3,6 @@ package clase.schimb_valutar;
 public class NewPaymentSystem implements INewPaymentSystem {
     @Override
     public void payInEUR(double amount) {
-        double euroAmount = amount*0.89;
-        System.out.println("The amount of " + euroAmount + " euros was successfully payed!");
+        System.out.println("The amount of " + amount + " euros was successfully payed!");
     }
 }
